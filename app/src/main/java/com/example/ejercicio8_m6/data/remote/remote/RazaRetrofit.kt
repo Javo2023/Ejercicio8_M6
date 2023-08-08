db7eaa3b1580307cdb00148987c2cea480036e00
javo2023 <jguzmanalarcon@gmail.com>
2023-08-07 22:21:35 -0400

    viewModel

--- app/src/main/java/com/example/ejercicio8_m6/data/remote/remote/RazaRetrofit.kt
+++ app/src/main/java/com/example/ejercicio8_m6/data/remote/remote/RazaRetrofit.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RazaRetrofit {
     companion object {
         private const val URL_BASE = "https://dog.ceo/api/"
-        fun gesRetroFitRaza(): RazaApi {
+        fun getRetrofitRaza(): RazaApi {
 
             val mRetrofit = Retrofit.Builder()
                 .baseUrl(URL_BASE)
---
+++ app/src/main/java/com/example/ejercicio8_m6/presentacion/RazaViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.ejercicio8_m6.presentacion
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ejercicio8_m6.data.remote.Repositorio
+import com.example.ejercicio8_m6.data.remote.local.RazaDatabase
+import com.example.ejercicio8_m6.data.remote.remote.RazaRetrofit
+import kotlinx.coroutines.launch
+
+class RazaViewModel (application: Application): AndroidViewModel(application){
+    private val repositorio:Repositorio
+    init{
+        val api = RazaRetrofit.getRetrofitRaza()
+        val razaDatabase = RazaDatabase.getDataBase(application).getRazaDao()
+        repositorio = Repositorio(api,razaDatabase)
+    }
+    fun getAllRazas() = viewModelScope.launch {
+        repositorio.getRazas()
+    }
+}
